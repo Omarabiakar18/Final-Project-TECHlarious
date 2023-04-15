501fbd7d61b0e4699549e779bf4cf03959f350a7
Omarabiakar18 <abiakaromar18@outlook.com>
2023-04-15 15:58:57 +0300

    Editing js files

--- QNA.js
+++ QNA.js
@@ -1,82 +1,145 @@
 module.exports = [
-    {
-        question: "How many guitars does Taylor Swift have?",
-        answers: ["13", "22", "She lost count in 2007"]
-    },
-    {
-        question: "First Song Taylor Swift learned on the guitar?",
-        answers: ["Kiss me by sixpence none the richer", "Our Song by Taylor Swift", "Blank Space by Taylor Swift"]
-    },
-    {
-        question: "What is Taylor Swift's favorite subject in school?",
-        answers: ["Math", "English", "Science"]
-    },
-    {
-        question: "What is Taylor Swift's favorite cocktail?",
-        answers: ["Vodka and diet coke", "Pina colada", "Negroni"]
-    },
-    {
-        question: "What is one thing Taylor Swift should always have in the fridge?",
-        answers: ["Humus", "Gluten free cookies", "Nuts"]
-    },
-    {
-        question: "When was Taylor Swift's debut album released?",
-        answers: ["October 24, 2006", "October 26, 2006", "October 24, 2007"]
-    },
-    {
-        question: "Which of the following are not released yet? (Last updated April 3, 2023)",
-        answers: ["Red (Taylor's Version)", "Fearless (Taylor's Version)", "Reputation (Taylor's Version)"]
-    },
-    {
-        question: "What is Taylor Swift's song about Jake Gyllenhaal?",
-        answers: ["All To Well (10 Minute Version) (Taylor's Version) (From the Vault)", "Style", "Lavender Haze"]
-    },
-    {
-        question: "Which song is not from Reputation?",
-        answers: ["So it goes", "The last great American dynasty", "…Ready For It?"]
-    },
-    {
-        question: "What Song is the opening act on The Era's Tour?",
-        answers: ["Lavender Haze", "Miss Americana and the heartbreak prince", "Cardigan"]
-    },
-    {
-        question: "Who stole Taylor Swift's songs?",
-        answers: ["Scooter Brown", "Jake Gyllenhaal", "John Mayer"]
-    },
-    {
-        question: "Who is the Best Singer in the world?",
-        answers: ["Taylor Swift", "Taylor Swift", "Taylor Swift"]
-    },
-    {
-        question: "Who Co-produced the album Midnights with Taylor Swift?",
-        answers: ["Max Martin", "Jack Antonoff", "Ryan Tedder"]
-    },
-    {
-        question: "What Song is the closing act on The Reputation Stadium Tour?",
-        answers: ["Dress", "Call It What You Want", "This Is Why We Can't Have Nice Things"]
-    },
-    {
-        question: "Who is Taylor Swift's Boyfriend?",
-        answers: ["Joe Alwyn", "Harry Styles", "John Mayer"]
-    },
-    {
-        question: "Who is Taylor Swift's Best Friend?",
-        answers: ["Selena Gomez", "Justin Bieber", "Hailey Bieber"]
-    },
-    {
-        question: "Which song is about Taylor Swift's mom?",
-        answers: ["ME! (feat. Brendon Urie of Panic! At The Disco) ft. Brendon Urie", "Soon You'll Get Better", "All Too Well (Taylor's Version)"]
-    },
-    {
-        question: "Who took the microphone from Taylor Swift on stage after winning the VMA awards in 2009?",
-        answers: ["Kanye West", "John Mayer", "Scooter Brown"]
-    },
-    {
-        question: "Which album is Taylor Swift's comeback after the Kanye West and Kim Kardashian drama?",
-        answers: ["Lover", "Reputation", "Evermore"]
-    },
-    {
-        question: "Which Album was released at the beginning of the pandemic in 2020?",
-        answers: ["Midnights", "Folklore", "Speak Now"]
-    }
-]
+  {
+    question: "How many guitars does Taylor Swift have?",
+    answers: ["13", "22", "She lost count in 2007"],
+    correct: 2,
+  },
+  {
+    question: "First Song Taylor Swift learned on the guitar?",
+    answers: [
+      "Kiss me by sixpence none the richer",
+      "Our Song by Taylor Swift",
+      "Blank Space by Taylor Swift",
+    ],
+    correct: 0,
+  },
+  {
+    question: "What is Taylor Swift's favorite subject in school?",
+    answers: ["Math", "English", "Science"],
+    correct: 1,
+  },
+  {
+    question: "What is Taylor Swift's favorite cocktail?",
+    answers: ["Vodka and diet coke", "Pina colada", "Negroni"],
+    correct: 0,
+  },
+  {
+    question:
+      "What is one thing Taylor Swift should always have in the fridge?",
+    answers: ["Gluten free cookies", "Humus", "Nuts"],
+    correct: 1,
+  },
+  {
+    question: "When was Taylor Swift's debut album released?",
+    answers: ["October 24, 2006", "October 26, 2006", "October 24, 2007"],
+    correct: 0,
+  },
+  {
+    question:
+      "Which of the following are not released yet? (Last updated April 13, 2023)",
+    answers: [
+      "Red (Taylor's Version)",
+      "Fearless (Taylor's Version)",
+      "Reputation (Taylor's Version)",
+    ],
+    correct: 2,
+  },
+  {
+    question: "Which Taylor Swift song is about Jake Gyllenhaal?",
+    answers: [
+      "All To Well (10 Minute Version) (Taylor's Version) (From the Vault)",
+      "Style",
+      "Lavender Haze",
+    ],
+    correct: 0,
+  },
+  {
+    question: "Which song is not from Reputation?",
+    answers: [
+      "So it goes",
+      "…Ready For It?",
+      "The last great American dynasty",
+    ],
+    correct: 2,
+  },
+  {
+    question: "What Song is the opening act on The Era's Tour?",
+    answers: [
+      "Lavender Haze",
+      "Miss Americana and the heartbreak prince",
+      "Cardigan",
+    ],
+    correct: 1,
+  },
+  {
+    question: "Who stole Taylor Swift's songs?",
+    answers: ["Jake Gyllenhaal", "John Mayer", "Scooter Brown"],
+    correct: 2,
+  },
+  {
+    question: "Who is the Best Singer in the world?",
+    answers: [
+      "Taylor Swift (choose this option)",
+      "Taylor Swift",
+      "Taylor Swift",
+    ],
+    correct: 0,
+  },
+  {
+    question: "Who Co-produced the album Midnights with Taylor Swift?",
+    answers: ["Max Martin", "Jack Antonoff", "Ryan Tedder"],
+    correct: 1,
+  },
+  {
+    question: "What Song is the closing act on The Reputation Stadium Tour?",
+    answers: [
+      "Dress",
+      "Call It What You Want",
+      "This Is Why We Can't Have Nice Things",
+    ],
+    correct: 2,
+  },
+  {
+    question:
+      "Who is Taylor Swift's Boyfriend? (I don't care about the rumors!!)",
+    answers: ["Joe Alwyn", "Harry Styles", "John Mayer"],
+    correct: 0,
+  },
+  {
+    question: "Who is Taylor Swift's Best Friend?",
+    answers: ["Selena Gomez", "Justin Bieber", "Hailey Bieber"],
+    correct: 0,
+  },
+  {
+    question: "Which song is about Taylor Swift's mom?",
+    answers: [
+      "ME! (feat. Brendon Urie of Panic! At The Disco) ft. Brendon Urie (Lover)",
+      "Soon You'll Get Better (Lover)",
+      "All Too Well (Taylor's Version)",
+    ],
+    correct: 1,
+  },
+  {
+    question:
+      "Who took the microphone from Taylor Swift on stage after winning the VMA awards in 2009?",
+    answers: ["Scooter Brown", "John Mayer", "Kanye West"],
+    correct: 2,
+  },
+  {
+    question:
+      "Which album is Taylor Swift's comeback after the Kanye West and Kim Kardashian drama?",
+    answers: ["Lover", "Evermore", "Reputation"],
+    correct: 2,
+  },
+  {
+    question:
+      "Which Album was released at the beginning of the pandemic in 2020?",
+    answers: ["Midnights", "Folklore", "Evermore"],
+    correct: 1,
+  },
+  {
+    question: "What Song is the closing act on The Era's Tour?",
+    answers: ["Lavender Haze", "Karma", "Illicit Affairs"],
+    correct: 1,
+  },
+];
